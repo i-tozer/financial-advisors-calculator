df34033b4fdf7171d73bdbf144a6daf185dc101a
Ian Tozer <23465548+i-tozer@users.noreply.github.com>
2025-03-22 20:45:45 +0000

    Add main application component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
